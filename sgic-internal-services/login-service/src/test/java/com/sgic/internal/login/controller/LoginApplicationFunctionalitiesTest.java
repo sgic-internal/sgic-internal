e82cfb056ea7ba80030c1ffb0618dc538d07839b
priyankamoorthy29 <priyankamoorthy29@gmail.com>
2019-06-20 09:48:43 +0530

    Added Junit Testing

--- sgic-internal-services/login-service/src/test/java/com/sgic/internal/login/controller/LoginApplicationFunctionalitiesTest.java
+++ sgic-internal-services/login-service/src/test/java/com/sgic/internal/login/controller/LoginApplicationFunctionalitiesTest.java
@@ -1,6 +1,7 @@
 package com.sgic.internal.login.controller;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 
@@ -13,9 +14,11 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClientException;
 
 import com.sgic.internal.login.LoginApplicationTests;
+import com.sgic.internal.login.entities.User;
 
 
 public class LoginApplicationFunctionalitiesTest extends LoginApplicationTests{
@@ -27,9 +30,8 @@ public class LoginApplicationFunctionalitiesTest extends LoginApplicationTests{
   public void setup() {
 	  
   
-  String newUser = "INSERT INTO login.user(email, password,role) VALUES ('hai@gmail.com', 'mathu','sharma')";
-
-  jdbcTemplate.execute(newUser);
+//  String newUser = "INSERT INTO login.user(email, password,role) VALUES ('pppp', 'ppp','ooo')";
+//  jdbcTemplate.execute(newUser);
 
   }
   
@@ -40,8 +42,37 @@ public class LoginApplicationFunctionalitiesTest extends LoginApplicationTests{
            new HttpEntity<>(httpHeaders), String.class);
    assertEquals(HttpStatus.OK, response.getStatusCode());
    }
+ 
+  @Test
+  public void getLoginUserByMailSuccessfull() throws IOException, RestClientException {
+	  User user = testRestTemplate.getForObject("http://localhost:8081/login" + "/getByUserMail/mathu@gmail.com", User.class);
+     assertNotNull(user);
+   }
   
+    @Test
+    public void createLoginUserDetails() throws IOException, RestClientException{
+    	User user= new User();
+    	user.setEmail("mathu");
+    	user.setPassword("m123");
+    	user.setRole("user");
+    	
+    	testRestTemplate.postForEntity("http://localhost:8081/login" + "/user",HttpMethod.POST, User.class);
+    }
     
+    @Test
+    public void deleteLoginUserDetail()throws IOException, RestClientException{
+    	String email ="mathu@gmail.com";
+    	User user = testRestTemplate.getForObject("http://localhost:8081/login" + "/user/"+email, User.class);
+    	assertNotNull(user);
+    	
+    	testRestTemplate.delete("http://localhost:8081/login" + "/user/"+email);
+    	try {
+    		
+    		user = testRestTemplate.getForObject("http://localhost:8081/login" + "/user/"+email, User.class);
+    	}catch(final HttpClientErrorException e) {
+    	  assertEquals(e.getStatusCode(),HttpStatus.NOT_FOUND);
+    	}
+    }
   
   @After
   public void tearDown() {
@@ -53,7 +84,7 @@ public class LoginApplicationFunctionalitiesTest extends LoginApplicationTests{
     public LoginApplicationTest() {
     }
     
-    private static final String CAREER_DEVELOPMENT_PLAN_RESPONSE =
+    private static final String LOGIN_APPLICATION_DETAILS =
         "[ { \\\"email\\\": \\\"iiiiiii\\\", \\\"password\\\": \\\"kala\\\", \\\"role\\\": \\\"user\\\" }, { \\\"email\\\": \\\"kala@gmail.com\\\", \\\"password\\\": \\\"kala\\\", \\\"role\\\": \\\"user\\\" }, { \\\"email\\\": \\\"mathu@gmail.com\\\", \\\"password\\\": \\\"one\\\", \\\"role\\\": \\\"user\\\" } ]";
 
   }
