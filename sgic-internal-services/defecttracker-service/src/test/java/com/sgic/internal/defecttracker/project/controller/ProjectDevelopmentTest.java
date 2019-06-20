5f07b5903a76e06f982252e102e86f5d3eff9775
Fathima Sajitha <fathi.saji8@gmail.com>
2019-06-20 15:32:49 +0530

    Unit Testing

--- sgic-internal-services/defecttracker-service/src/test/java/com/sgic/internal/defecttracker/project/controller/ProjectDevelopmentTest.java
+++ sgic-internal-services/defecttracker-service/src/test/java/com/sgic/internal/defecttracker/project/controller/ProjectDevelopmentTest.java
@@ -1,8 +1,10 @@
 package com.sgic.internal.defecttracker.project.controller;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
+import java.sql.Date;
 
 import org.junit.After;
 import org.junit.Before;
@@ -13,51 +15,141 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClientException;
 
 import com.sgic.internal.defecttracker.project.ProjectApplicationTest;
+import com.sgic.internal.defecttracker.project.entities.Project;
+
+public class ProjectDevelopmentTest extends ProjectApplicationTest {
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Before
+	public void setup() {
+
+	}
+
+@Test
+	public void GetProjectById() throws IOException, RestClientException {
+
+		String newUser = "INSERT INTO project_service.project (project_id, config_id, duration, end_date, project_name, start_date, status, type)VALUES (5,6,'15','2019-05-05','Thanu','2019-06-05','open','High')";
+		
+		jdbcTemplate.execute(newUser);
+		
+
+		ResponseEntity<String> response = testRestTemplate.exchange(
+				"http://localhost:8080/project_service" + "/getProjectById/1", HttpMethod.GET,
+				new HttpEntity<>(httpHeaders), String.class);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
+		@Test
+		public void GetAllProject() {
+			ResponseEntity<String> response1 = testRestTemplate.exchange(
+					"http://localhost:8080/project_service" + "/GetAllproject", HttpMethod.GET,
+					new HttpEntity<>(httpHeaders), String.class);
+			assertEquals(HttpStatus.OK, response1.getStatusCode());
+		}
 
 
+	@Test
+	public void GetProjectByName() {
+		ResponseEntity<String> response2 = testRestTemplate.exchange(
+				"http://localhost:8080/project_service" + "/getName/jakee", HttpMethod.GET,
+				new HttpEntity<>(httpHeaders), String.class);
+		assertEquals(HttpStatus.OK, response2.getStatusCode());
+	}
+	@Test
+	public void GetProjectByStatus() {
+		ResponseEntity<String> response3 = testRestTemplate.exchange(
+				"http://localhost:8080/project_service" + "/getstatus/Mercy", HttpMethod.GET,
+				new HttpEntity<>(httpHeaders), String.class);
+		assertEquals(HttpStatus.OK, response3.getStatusCode());
+	}
+	@Test
+	public void GetProjectByType() {
+		ResponseEntity<String> response3 = testRestTemplate.exchange(
+				"http://localhost:8080/project_service" + "/gettype/Mercy", HttpMethod.GET,
+				new HttpEntity<>(httpHeaders), String.class);
+		assertEquals(HttpStatus.OK, response3.getStatusCode());
+	}
+	@Test
+	public void GetProjectByduration() {
+		ResponseEntity<String> response3 = testRestTemplate.exchange(
+				"http://localhost:8080/project_service" + "/getduration/Mercy", HttpMethod.GET,
+				new HttpEntity<>(httpHeaders), String.class);
+		assertEquals(HttpStatus.OK, response3.getStatusCode());
 
-public class ProjectDevelopmentTest extends ProjectApplicationTest{
+	}
+
+	@Test
+	public void PostProjectDevelopmentTestSuccessfull() throws IOException, RestClientException {
+
+		Project project = new Project();
+		Date endDate=new Date(2018, 07, 12);
+		Date startDate=new Date(2019, 02, 13);
+//		endDate.setYear(2018);
+//		endDate.setMonth(07);
+//		endDate.setDate(12);
+		project.setProjectId((long) 1);
+		project.setProjectName("Jakeerththana");
+		project.setDuration("two hours");
+		project.setEndDate(endDate);
+		project.setStatus("open");
+		project.setType("high");
+		project.setStartDate(startDate);
+		testRestTemplate.postForEntity("http://localhost:8080/project_service" + "/createproject", project,Project.class);
+
+	}
 	
-	 @Autowired
-	 JdbcTemplate jdbcTemplate;
-	 
-	 @Before
-	 public void setup() {
-	 
-	 
-	 String newUser = "INSERT INTO project_service.project (project_id, config_id, duration,end_date,project_name,start_date,status,type)VALUES ('5', 'sajit','ansar','2019-06-12','ss','2019-06-12','ss','ff')";
-	 //String careerDevelopmentPlan = "INSERT INTO employee.careerdevelopmentplan (id, name) VALUES (2, 'careerDevelopmentOne')";
-	  //String userCareerDevelopmentPlanCompany = "INSERT INTO employee.user_career_development_plan_company (id, cdp_Id,user_Id,status) VALUES (2,2,2,'Planning' )";
-	 jdbcTemplate.execute(newUser);
-	  //jdbcTemplate.execute(careerDevelopmentPlan);
-	   //jdbcTemplate.execute(userCareerDevelopmentPlanCompany);
-	   
-	 }
-	 
+	@Test
+	public void updateProjectupdateTest() throws IOException, RestClientException {
+		
+		int projectid =1;
+		ResponseEntity<Project> project = testRestTemplate.getForEntity("http://localhost:8080/project_service" + "/getProjectById/" + projectid,Project.class);
+		assertNotNull(project);
+		Project project2 = new Project();
+		Date endDate=new Date(2018, 07, 12);
+		Date startDate=new Date(2019, 02, 13);
+		project2.setConfigId("1");
+		project2.setDuration("1");
+		project2.setEndDate(startDate);
+		project2.setProjectName("xx");
+		project2.setStartDate(endDate);
+		project2.setStatus("dd");
+		project2.setType("oo");
+		testRestTemplate.put("http://localhost:8080/project_service/"+"updateProject/"+projectid, project2, Project.class);
+		
+	}
+	private String getRootUrl() {
+		// TODO Auto-generated method stub
+		return "http://localhost:8080/project_service";
+	}
 	 @Test
-	 public void getProjectDevelopmentTestSuccessfull() throws IOException, RestClientException {
-	   ResponseEntity<String> response =
-	       testRestTemplate.exchange("http://localhost:8080/project_service" + "/ViewProject", HttpMethod.GET,
-	           new HttpEntity<>(httpHeaders), String.class);
-	   assertEquals(HttpStatus.OK, response.getStatusCode());
-	  }
-	   
-	 
-	 @After
-	 public void tearDown() {
-
-	 }
-	 
-	  public final class ProjectDevelopmentTestConstant{
-	    
-	    public ProjectDevelopmentTestConstant() {
+	    public void DeleteProjectTest() throws IOException,RestClientException {
+	         int id = 1;
+	         Project project = testRestTemplate.getForObject(getRootUrl() + "/deleteById/" + id, Project.class);
+	         assertNotNull(project);
+	         testRestTemplate.delete(getRootUrl() + "/project/" + id);
+	         try {
+	              project = testRestTemplate.getForObject(getRootUrl() + "/deleteById/" + id, Project.class);
+	         } catch (final HttpClientErrorException e) {
+	              assertEquals(e.getStatusCode(), HttpStatus.NOT_FOUND);
+	         }
 	    }
-	    
-	    private static final String PROJECT_DEVELOPMENT_TEST_RESPONSE =
-	        "[{ \"projectId\": 1, \"projectName\": \"ggggjh\", \"type\": \"spring\", \"startDate\": \"2018-07-12\", \"endDate\": \"2019-02-16\", \"duration\": \"2 year\", \"status\": \"open\", \"configId\": null }]";
-	  }
+
+	@After
+	public void tearDown() {
+
+	}
+
+	public final class ProjectDevelopmentTestConstant {
+
+		public ProjectDevelopmentTestConstant() {
+		}
+
+		private static final String PROJECT_DEVELOPMENT_TEST_RESPONSE = "[ { \\\"projectId\\\": 1, \\\"projectName\\\": \\\"jakee\\\", \\\"type\\\": \\\"fytvh\\\", \\\"startDate\\\": \\\"2018-07-12\\\", \\\"endDate\\\": \\\"2018-02-16\\\", \\\"duration\\\": \\\"2 year\\\", \\\"status\\\": \\\"open\\\", \\\"configId\\\": \\\"011\\\" } ]";
+	}
 
 }
