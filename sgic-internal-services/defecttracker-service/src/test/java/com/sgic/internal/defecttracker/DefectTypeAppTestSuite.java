4cccace7112eaa233bffcc0d74a02a8adb2faa7d
MathuraSivarajah <smadhura94@gmail.com>
2019-06-23 17:07:42 +0530

    Unit Testing Updated

--- sgic-internal-services/defecttracker-service/src/test/java/com/sgic/internal/defecttracker/DefectTypeAppTestSuite.java
+++ sgic-internal-services/defecttracker-service/src/test/java/com/sgic/internal/defecttracker/DefectTypeAppTestSuite.java
@@ -3,10 +3,20 @@ package com.sgic.internal.defecttracker;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import com.sgic.internal.defecttracker.controller.CreateDefectTypeTest;
+import com.sgic.internal.defecttracker.controller.DeleteDefectTypeTest;
+import com.sgic.internal.defecttracker.controller.GetAllDefectTypesTest;
+import com.sgic.internal.defecttracker.controller.GetByIdDefectTypeTest;
+import com.sgic.internal.defecttracker.controller.UpdateDefectTypeTest;
+
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-	DefectTypeAppTest.class
+	CreateDefectTypeTest.class,
+	GetAllDefectTypesTest.class,
+	GetByIdDefectTypeTest.class,
+	DeleteDefectTypeTest.class,
+	UpdateDefectTypeTest.class
 })
 public class DefectTypeAppTestSuite {
 
