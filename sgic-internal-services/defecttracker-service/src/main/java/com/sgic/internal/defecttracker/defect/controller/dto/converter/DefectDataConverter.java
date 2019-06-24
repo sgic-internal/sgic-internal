df845b93cd0bb199af63de851ba97f4866bebbc9
priyankamoorthy29 <priyankamoorthy29@gmail.com>
2019-06-24 10:57:02 +0530

    nxt commit

--- sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/defect/controller/dto/converter/DefectDataConverter.java
+++ sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/defect/controller/dto/converter/DefectDataConverter.java
@@ -82,7 +82,7 @@ public class DefectDataConverter {
 	
 //========================To convert DefectDTO to DefectEntity =====================================================
 	
-	public static Defect UserDataToUser(DefectData defectData) {
+	public static Defect DefectDataToDefect(DefectData defectData) {
 		Defect defect = new Defect();
 		if (defectData != null) {
 			logger.info("Coverting DefectData to DefectEntity");
--- sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/defect/controller/dto/mapper/DefectDataMapper.java
+++ sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/defect/controller/dto/mapper/DefectDataMapper.java
@@ -1,5 +1,35 @@
 package com.sgic.internal.defecttracker.defect.controller.dto.mapper;
 
-public class DefectDataMapper {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import com.sgic.internal.defecttracker.defect.controller.dto.DefectData;
+import com.sgic.internal.defecttracker.defect.controller.dto.converter.DefectDataConverter;
+import com.sgic.internal.defecttracker.defect.entities.Defect;
+import com.sgic.internal.defecttracker.defect.services.DefectService;
+
+@Service
+public class DefectDataMapper {
+	
+	@Autowired
+	private DefectService defectService;
+
+	@Autowired
+	private DefectDataConverter defectDataConverter;
+	
+	private static Logger logger = LogManager.getLogger(DefectData.class);
+	
+
+//===========================Creating New defects=================================
+	
+	@SuppressWarnings("static-access")
+	public  Defect createDefect(DefectData defectData) {
+		logger.info("DefectData Mapper -> Defect Details Saved");
+		Defect defect = defectDataConverter.DefectDataToDefect(defectData);
+		return defectService.createDefect(defect);
+		
+	}
+	
 }
