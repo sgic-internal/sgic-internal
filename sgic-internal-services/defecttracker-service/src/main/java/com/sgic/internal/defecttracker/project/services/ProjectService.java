e4f45c2a678dab28e3b8b654945b16d53656abc2
ramz <ramminarayanan7@gmail.com>
2019-06-25 17:37:08 +0530

    final commit

--- sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/project/services/ProjectService.java
+++ sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/project/services/ProjectService.java
@@ -1,21 +1,5 @@
 package com.sgic.internal.defecttracker.project.services;
 
-
-import java.util.List;
-
-import com.sgic.internal.defecttracker.project.entities.Project;
-
 public interface ProjectService {
-	public Project createProject(Project project);
-	public boolean isProjectAlreadyExists(String id);
-	public List<Project>getallDetails();
-	public void deleteById(String id);
-	public Project updateProject(String projectid, Project project);  //Update projectid
-	Project getByprojectId(String id);
-	public List<Project>getByprojectName (String name);
-	public List<Project>getBytype (String type);
-	public List<Project>getBystartDate (String date);
-	public List<Project>getByduration (String duration);
-	public List<Project>getBystatus (String status);
 
 }
--- sgic-internal-services/defecttracker-service/src/main/main/java/com/sgic/internal/defecttracker/defect/repositories/DefectRepository.java
+++
@@ -1,5 +0,0 @@
-package com.sgic.internal.defecttracker.defect.repositories;
-
-public interface DefectRepository {
-
-}
