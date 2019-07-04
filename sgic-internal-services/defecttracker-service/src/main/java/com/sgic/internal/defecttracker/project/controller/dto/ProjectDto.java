4718968273cfc591914245bb83000baf2c1c024b
ramz <ramminarayanan7@gmail.com>
2019-07-04 17:31:59 +0530

    release-3

--- sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/project/controller/dto/ProjectDto.java
+++ sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/project/controller/dto/ProjectDto.java
@@ -2,16 +2,12 @@ package com.sgic.internal.defecttracker.project.controller.dto;
 
 public class ProjectDto {
 
-	private Long pid;
+	
 	private String projectId;
+	
 	private String projectName;
 	
-	public Long getPid() {
-		return pid;
-	}
-	public void setPid(Long pid) {
-		this.pid = pid;
-	}
+
 	public String getProjectId() {
 		return projectId;
 	}
