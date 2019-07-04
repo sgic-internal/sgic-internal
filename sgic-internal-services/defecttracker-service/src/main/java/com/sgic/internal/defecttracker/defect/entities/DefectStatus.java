e5864cac1b96fd50c3f4236149a4ab1c22e1caad
unknown <varnitha94@gmail.com>
2019-07-04 19:58:27 +0530

    updated entity status

--- sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/defect/entities/DefectStatus.java
+++ sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/defect/entities/DefectStatus.java
@@ -2,13 +2,18 @@ package com.sgic.internal.defecttracker.defect.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+
 
 @SuppressWarnings("serial")
+
 @Entity
 @Table(schema = "defectservices", name = "defectstatus")
 public class DefectStatus implements Serializable {
@@ -16,9 +21,10 @@ public class DefectStatus implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id; // Defect Status Id
-
+	@Column(unique=true)
+	@NotEmpty
 	private String name; // Defect Status name
-
+	@NotEmpty
 	private String value; // Defect Status value
 
 	public long getId() {
