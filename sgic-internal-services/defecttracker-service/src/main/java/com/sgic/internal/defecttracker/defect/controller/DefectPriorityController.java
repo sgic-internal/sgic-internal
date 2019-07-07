cba610ec0c2efac64c0b02bf59dccb35ae45d169
MathuraSivarajah <smadhura94@gmail.com>
2019-07-07 14:51:31 +0530

    Priority and Severity controllers updated

--- sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/defect/controller/DefectPriorityController.java
+++ sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/defect/controller/DefectPriorityController.java
@@ -8,6 +8,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.sgic.internal.defecttracker.defect.controller.dto.DefectPriorityDto;
 import com.sgic.internal.defecttracker.defect.controller.dto.mapper.DefectPriorityMapper;
 import com.sgic.internal.defecttracker.defect.services.impl.DefectPriorityServiceImpl;
 
+@CrossOrigin
 @RestController
 public class DefectPriorityController {
 	
--- sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/defect/controller/DefectSeverityController.java
+++ sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/defect/controller/DefectSeverityController.java
@@ -8,6 +8,7 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.sgic.internal.defecttracker.defect.controller.dto.mapper.DefectSeveri
 import com.sgic.internal.defecttracker.defect.services.impl.DefectSeverityServiceImpl;
 
 @RestController
+@CrossOrigin
 public class DefectSeverityController {
 	private static Logger logger = LogManager.getLogger(DefectSeverityServiceImpl.class);
 	
