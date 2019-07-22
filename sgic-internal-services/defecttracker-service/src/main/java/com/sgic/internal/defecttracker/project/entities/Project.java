5835c5bcacfd48c92c82e9d1c88bcb48095ace21
romipraveen1 <romipraveen100@gmail.com>
2019-07-22 10:33:25 +0530

    Role entity added

--- sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/project/entities/Project.java
+++ sgic-internal-services/defecttracker-service/src/main/java/com/sgic/internal/defecttracker/project/entities/Project.java
@@ -69,15 +69,11 @@ public class Project implements Serializable {
 		return projectName;
 	}
 
-<<<<<<< HEAD
+
 	public void setProjectName(String projectName) {
 		this.projectName = projectName;
-=======
-	public void setProjectName(String value) {
-		this.projectName = value.trim();
->>>>>>> release5
-	}
 
+	}
 	public String getType() {
 		return type;
 	}
